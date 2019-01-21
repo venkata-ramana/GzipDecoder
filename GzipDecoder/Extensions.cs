a0e68c3de96a5451c65997a73405865225b067f0
Venkata Ramana Neelapala <venkata.r@technovert.con>
2019-01-21 12:17:51 +0530

    Added required files

---
+++ GzipDecoder/Extensions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GzipDecoder
+{
+    public static class Extensions
+    {
+        public static byte[] Zip(this string str)
+        {
+            var bytes = Encoding.UTF8.GetBytes(str);
+
+            using (var msi = new MemoryStream(bytes))
+            {
+                using (var mso = new MemoryStream())
+                {
+                    using (var gs = new GZipStream(mso, CompressionMode.Compress))
+                    {
+                        msi.CopyTo(gs);
+                    }
+
+                    return mso.ToArray();
+                }
+            }
+        }
+
+
+        public static string ZipContent(this string str)
+        {
+            return Convert.ToBase64String(Zip(str));
+        }
+
+        public static string Unzip(byte[] bytes)
+        {
+            using (var msi = new MemoryStream(bytes))
+            {
+                using (var mso = new MemoryStream())
+                {
+                    using (var gs = new GZipStream(msi, CompressionMode.Decompress))
+                    {
+                        gs.CopyTo(mso);
+                    }
+
+                    return Encoding.UTF8.GetString(mso.ToArray());
+                }
+            }
+        }
+
+        public static string UnzipContent(this string encodedContent)
+        {
+            return Unzip(Convert.FromBase64String(encodedContent));
+        }
+    }   
+}
---
+++ GzipDecoder/Program.cs
@@ -0,0 +1,21 @@
+﻿using Elmah;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GzipDecoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = "##";
+            var error = str.UnzipContent();
+            Console.Clear();
+            Console.WriteLine(error);
+            Console.ReadKey();
+        }
+    }
+}
